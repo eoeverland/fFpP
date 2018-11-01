1bf9a95acc85f849708e44e60eb073f55da38006
Edvard Øverland <edv.overland@gmail.com>
2018-11-01 18:28:56 +0100

    Klar 2player
    Klart med 2player, kjedelig gameplay.
    TODO:
    - Vector-implementasjon
    - bug der p2's y-vel sender den ut av rammen ved modusbytte.
    - telefonkompatibilitet.
    - Hinder?

--- js/pong.js
+++ js/pong.js
@@ -1,80 +1,89 @@
+let canvas = document.getElementById('pong');
+const context = canvas.getContext('2d');
+
 class Vec {
-  constructor(x=0, y=0){
+  constructor(x = 0, y = 0) {
     this.x = x;
     this.y = y;
   }
 }
 
 class Rect {
-  constructor(w,h) {
+  constructor(w, h) {
     this.pos = new Vec;
-    this.size = new Vec(w,h);
-    this.draw = function(color='#fff'){
+    this.size = new Vec(w, h);
+    this.draw = function(color = '#fff') {
       context.fillStyle = color;
       context.fillRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
     }
   }
-  get left(){
+  get left() {
     return this.pos.x - this.size.x / 2;
   }
-  get right(){
+  get right() {
     return this.pos.x + this.size.x / 2;
   }
-  get top(){
+  get top() {
     return this.pos.y;
   }
-  get bottom(){
+  get bottom() {
     return this.pos.y + this.size.y;
   }
 }
 
-class Paddle extends Rect{
-  constructor(side){
-    super(10,100);
-    this.pos.y = canvas.height/2 - this.size.y/2;
-    if(side == 'right'){
-      this.pos.x = canvas.width - 30 - this.size.x/2;
-    }else if(side == 'left'){
+class Paddle extends Rect {
+  constructor(side) {
+    super(10, 100);
+    this.pos.y = canvas.height / 2 - this.size.y / 2;
+    if (side == 'right') {
+      this.pos.x = canvas.width - 30 - this.size.x / 2;
+    } else if (side == 'left') {
       this.pos.x = 30;
     }
     this.vel = new Vec;
+    this.velocity = 200;
+    this.update = function(dt) {
+      this.pos.x += this.vel.x * dt;
+      this.pos.y += this.vel.y * dt;
+    }
   }
 }
 
 class Ball extends Rect {
-  constructor(x,y){
-    super(10,10);
+  constructor(x, y) {
+    super(10, 10);
     this.vel = new Vec;
+    this.update = function(dt) {
+      this.pos.x += this.vel.x * dt;
+      this.pos.y += this.vel.y * dt;
+    }
   }
 }
 
 class Spark extends Rect {
-  constructor(_xspeed, _yspeed){
-    super(Math.random() * (-3, - 7) + 7, Math.random() * (-3, - 7) + 7);
+  constructor(_xspeed, _yspeed) {
+    super(Math.random() * (-3, -7) + 7, Math.random() * (-3, -7) + 7);
+    this.vel = new Vec(_xspeed, _yspeed);
     this.pos.x = ball.pos.x + 3;
     this.pos.y = ball.pos.y + 2;
-    this.xspeed = _xspeed;
-    this.yspeed = _yspeed;
+    this.update = function(dt) {
+      this.pos.x += this.vel.x * dt;
+      this.pos.y += this.vel.y * dt;
+    }
   }
 }
 
-const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
-
 let ball = new Ball;
-ball.pos.y = canvas.height/2;
-ball.pos.x = canvas.width/2;
+ball.pos.y = canvas.height / 2;
+ball.pos.x = canvas.width / 2;
 
 const p1 = new Paddle('left');
 const p2 = new Paddle('right');
-p1.speed = 350;
-p2.speed = 200;
 let ai = true;
 let stopped = true;
 
 let home = 0;
 let away = 0;
-let round = 0;
 let counter = 0;
 let sparking = false;
 let sparks = [];
@@ -82,45 +91,50 @@ let audio = [];
 let hitCounter = 0;
 
 let sprites = [];
-sprites.push(p1,p2,ball);
+sprites.push(p1, p2, ball);
 
-function loadAudio(){
-  for(i = 0; i < 4; i++){
+function loadAudio() {
+  for (i = 0; i < 4; i++) {
     let src = 'media/audio/Computer_Data_0' + i + '.m4a';
     audio[i] = new Audio(src);
   }
 }
 
-function createSparks(amount){
-  for(i = 0; i < amount; i++){
+function createSparks(amount) {
+  for (i = 0; i < amount; i++) {
     let speed = 9;
     sparks[i] = new Spark(Math.random() * (-speed - speed) + speed, Math.random() * (-speed - speed) + speed);
-    setTimeout(function(){sparks = [];}, 500);
+    setTimeout(function() {
+      sparks = [];
+    }, 500);
   }
 }
 
-function hit(amount=7,){
+function hit(amount = 7, ) {
   hitCounter += 1;
   audio[1].play();
   createSparks(amount);
   sparking = true;
-  setTimeout(function(){sparking = false;}, 800);
+  setTimeout(function() {
+    sparking = false;
+  }, 800);
 }
 
-function updateSparks(){
-  if(sparking){
-    for(i = 0; i < sparks.length; i++){
-      sparks[i].pos.x += sparks[i].xspeed;
-      sparks[i].pos.y += sparks[i].yspeed;
-      sparks[i].xspeed *= 0.93;
-      sparks[i].yspeed *= 0.93;
+function updateSparks() {
+  if (sparking) {
+    for (i = 0; i < sparks.length; i++) {
+      sparks[i].pos.x += sparks[i].vel.x;
+      sparks[i].pos.y += sparks[i].vel.y;
+      sparks[i].vel.x *= 0.93;
+      sparks[i].vel.y *= 0.93;
     }
   }
 }
 
 let lastTime;
-function callback(millis){
-  if(lastTime){
+
+function callback(millis) {
+  if (lastTime) {
     update((millis - lastTime) / 1000);
   }
   lastTime = millis;
@@ -129,35 +143,35 @@ function callback(millis){
 
 let canvasColor = 'rgb(0,118,229)';
 
-function draw(){
+function draw() {
   // draw canvas
   context.fillStyle = canvasColor;
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  for(i = 0; i < sprites.length; i++){
+  for (i = 0; i < sprites.length; i++) {
     sprites[i].draw();
   }
 
-  if(sparking){
-    for(i = 0; i < sparks.length; i++){
+  if (sparking) {
+    for (i = 0; i < sparks.length; i++) {
       sparks[i].draw('#FFFF33');
     }
   }
 
 }
 
-function resetBall(){
+function resetBall() {
   ball.vel.x = 0;
   ball.vel.y = 0;
-  ball.pos.x = canvas.width/2;
-  ball.pos.y = canvas.height/2;
-  setTimeout(function(){
+  ball.pos.x = canvas.width / 2;
+  ball.pos.y = canvas.height / 2;
+  setTimeout(function() {
     ball.vel.x = 350;
-    if(fiftyFifty()){
+    if (fiftyFifty()) {
       ball.vel.x *= -1;
     }
     ball.vel.y = 145;
-    if(fiftyFifty()){
+    if (fiftyFifty()) {
       ball.vel.y *= -1;
     }
   }, 700);
@@ -166,174 +180,181 @@ function resetBall(){
 
 var velX; // lagrer ball sin x-velocity under pause
 var velY; // lagrer ball sin y-velocity under pause
-function startStop(){
-  if(ball.vel.x || ball.vel.y){
+function startStop() {
+  if (ball.vel.x || ball.vel.y) {
     velX = ball.vel.x;
     velY = ball.vel.y;
     ball.vel.x = 0;
     ball.vel.y = 0;
     stopped = true;
-  }else{
-    if(stopped){
+  } else {
+    if (stopped) {
       ball.vel.x = velX || 500;
       ball.vel.y = velY || 133;
       stopped = false;
-    }else{
+    } else {
       resetBall();
     }
   }
 }
-
-function fiftyFifty(){
-  if(Math.random(1) >= .5){
-    return 0;
-  } else {
-    return 1;
-  }
+// ternary operator chooses randomly 0 || 1
+function fiftyFifty() {
+  return Math.random(1) >= .5 ? 0 : 1;
 }
 
-function changeDirection(b){
+function changeDirection(b) {
   b.vel.x *= -1;
 }
 
-function collide(){
+function collide() {
   // collide player1
-  if(ball.bottom > p1.top && ball.top < p1.bottom && ball.left < p1.right && ball.right > p1.left && ball.vel.x < 0){
+  if (ball.bottom > p1.top && ball.top < p1.bottom && ball.left < p1.right && ball.right > p1.left && ball.vel.x < 0) {
     return true;
   }
   // collide player2
-  if(ball.bottom > p2.top && ball.top < p2.bottom && ball.right > p2.left && ball.left < p2.right && ball.vel.x > 0){
+  if (ball.bottom > p2.top && ball.top < p2.bottom && ball.right > p2.left && ball.left < p2.right && ball.vel.x > 0) {
     return true;
   }
 }
 
-function wallDetect(){
+function wallDetect() {
   //topp & bunn
-  if(ball.top < 0 && ball.vel.y < 0){
+  if (ball.top < 0 && ball.vel.y < 0) {
     ball.vel.y *= -1;
   }
-  if(ball.bottom > canvas.height && ball.vel.y > 0){
+  if (ball.bottom > canvas.height && ball.vel.y > 0) {
     ball.vel.y *= -1;
   }
   // AI goal
-  if(ball.left < 0){
-    away ++;
-    round ++;
-    document.getElementById('p2').innerHTML = "Duplex: " + away;
+  if (ball.left < 0) {
+    away++;
+    if (ai) {
+      document.getElementById('p2').innerHTML = "AI: " + away;
+    } else {
+      document.getElementById('p2').innerHTML = "Player2: " + away;
+    }
     audio[3].play();
     resetBall();
     canvasColor = 'rgb(250,0,70)';
-    setTimeout(function(){
+    setTimeout(function() {
       canvasColor = 'rgb(0,118,229)';
-    } , 200);
+    }, 200);
   }
   // player goal
-  if(ball.right > canvas.width){
-    home ++;
-    round ++;
-    counter ++;
-    document.getElementById('p1').innerHTML = "You: " + home;
+  if (ball.right > canvas.width) {
+    home++;
+    counter++;
+    if (ai) {
+      document.getElementById('p1').innerHTML = "You: " + home;
+    } else {
+      document.getElementById('p1').innerHTML = "Player1: " + home;
+    }
     audio[0].play();
     resetBall();
     canvasColor = 'rgb(0,230,90)';
-    setTimeout(function(){
+    setTimeout(function() {
       canvasColor = 'rgb(0,118,229)';
-    } , 200);
+    }, 200);
   }
 }
 
-function blinkScreen(color){
+function blinkScreen(color) {
   canvasColor = color;
-  setTimeout(function(){
+  setTimeout(function() {
     canvasColor = 'rgb(0,118,229)';
-  } , 200);
+  }, 200);
 }
 
-function checkBoundaries(){
+function checkBoundaries() {
   wallDetect();
-  if(collide()){
+  if (collide()) {
     // check if the ball is on the top quarter of the paddle
-    if(ball.bottom < p1.pos.y + p1.size.y/4 || ball.bottom < p2.pos.y + p2.size.y/2){
-      ball.vel.y -=85;
+    if (ball.bottom < p1.pos.y + p1.size.y / 4 || ball.bottom < p2.pos.y + p2.size.y / 2) {
+      ball.vel.y -= 85;
       hit(15);
     }
     // check if the ball is on the top half of the paddle
-     else if(ball.bottom < p1.pos.y + p1.size.y/2 || ball.bottom < p2.pos.y + p2.size.y/2){
-      ball.vel.y -=45;
+    else if (ball.bottom < p1.pos.y + p1.size.y / 2 || ball.bottom < p2.pos.y + p2.size.y / 2) {
+      ball.vel.y -= 45;
       hit();
     }
     // check if the ball is on the bottom half of the paddle
-    if(ball.top > p1.pos.y + p1.size.y/4 || ball.top > p2.pos.y + p2.size.y/4){
-      ball.vel.y +=85;
+    if (ball.top > p1.pos.y + p1.size.y / 4 || ball.top > p2.pos.y + p2.size.y / 4) {
+      ball.vel.y += 85;
       hit();
     }
-      // check if the ball is on the bottom quarter of the paddle
-     else if(ball.top > p1.pos.y + p1.size.y/2 || ball.top > p2.pos.y + p2.size.y/2){
-      ball.vel.y +=45;
+    // check if the ball is on the bottom quarter of the paddle
+    else if (ball.top > p1.pos.y + p1.size.y / 2 || ball.top > p2.pos.y + p2.size.y / 2) {
+      ball.vel.y += 45;
       hit(15);
     }
-  ball.vel.x += hitCounter * 0.2 
-  changeDirection(ball);
+    ball.vel.x += hitCounter * 0.3;
+    changeDirection(ball);
   }
 }
 
-function newGame(){
+function newGame() {
   resetBall();
   counter = 0;
   away = 0;
   home = 0;
-  p2.speed = 200;
+  p2.velocity = 200;
 }
 
-function AI(dt){
-  if(ai){
+function AI(dt) {
+  if (ai) {
     // kontrollør, ai's paddle kan ikke bevege seg fortere enn ballen's y-fart
-    if(p2.speed > ball.vel.y){
+    if (p2.velocity > ball.vel.y) {
       p2.vel.y = ball.vel.y;
     }
-    if(ball.pos.x > canvas.width/3){
-      if(ball.pos.y < p2.top + 20){
-        p2.vel.y = -p2.speed;
-      }else if(ball.pos.y > p2.bottom - 20) {
-        p2.vel.y = p2.speed;
-      }else {
-        p2.vel.y = 0;
-      }
-    } else if (ball.pos.x < canvas.width/3){
-      if(p2.pos.y + p2.size.y/2 > canvas.height/2 + 10){
-          p2.vel.y = -p2.speed;
-      }else if(p2.pos.y + p2.size.y/2 < canvas.height/2 -10){
-        p2.vel.y = p2.speed;
+    if (ball.pos.x > canvas.width / 3) { // If the ball is on the right two thirds of the canvas.
+      if (ball.pos.y < p2.top + 20) { // ball is higher than p2's top 20px
+        p2.vel.y = -p2.velocity; // p2 goes up
+      } else if (ball.pos.y > p2.bottom - 20) { // Ball is lower than p2's bottom 20px
+        p2.vel.y = p2.velocity; // p2 goes down
       } else {
-        p2.vel.y = 0;
+        p2.vel.y = 0; // ball doesn't move
+      }
+    } else if (ball.pos.x < canvas.width / 3 || ball.vel.x < 1) { // ball is on the left third of the canvas.
+      if (p2.pos.y + p2.size.y / 2 > canvas.height / 2 + 10) { // p2's center is 10px lower than the center
+        p2.vel.y = -p2.velocity; // move up
+      } else if (p2.pos.y + p2.size.y / 2 < canvas.height / 2 - 10) { // p2's center is higher than the center of canvas.
+        p2.vel.y = p2.velocity; // Move down
+      } else {
+        p2.vel.y = 0; // don't move if the p2's center is within 10px of the canvas center.
       }
     }
 
-    p2.pos.y += p2.vel.y * dt;
+    p2.pos.y += p2.vel.y * dt; // update p2
   }
 }
 
 // oppdaterer elementer sin posisjon på siden
-function update(dt){
+function update(dt) {
   // ball sin bevegelse
   ball.pos.x += ball.vel.x * dt;
   ball.pos.y += ball.vel.y * dt;
 
-  p1.pos.x += p1.vel.x * dt;
   p1.pos.y += p1.vel.y * dt;
 
-  setTimeout(function(){AI(dt)},100);
+  p2.pos.y += p2.vel.y * dt;
 
-  if(sparking){
-    updateSparks(dt);
+  if (ai) {
+    setTimeout(function() {
+      AI(dt)
+    }, 200);
   }
 
   // Increase the botspeed every 3rd point.
-  if(counter%3 == 0 && counter != 0){
-    if(p2.speed < 600){
-      p2.speed *= 1.3;
-    counter++;
+  if (counter % 3 == 0 && counter != 0 && ai) {
+    if (p2.velocity < 600) {
+      p2.velocity *= 1.3;
+      counter++;
     }
+  }
+
+  if (sparking) {
+    updateSparks(dt);
   }
 
   draw();
--- js/pong_eventHandler.js
+++ js/pong_eventHandler.js
@@ -23,36 +23,55 @@ window.addEventListener('keypress', function(pressed) {
   if(pressed.key == 'a'){
     if(ai){
       ai = false;
+      document.getElementById('gameMode').innerHTML = "2 Player";
       console.log("I'll be back.");
+      document.getElementById('p1').innerHTML = "Player1: " + '0';
+      document.getElementById('p2').innerHTML = "Player2: " + '0';
+      newGame();
     }else {
       ai = true;
+      document.getElementById('gameMode').innerHTML = "You vs. AI";
       console.log('En garde!');
+      document.getElementById('p1').innerHTML = "You: " + '0';
+      document.getElementById('p2').innerHTML = "AI: " + '0';
+      newGame();
     }
   }
 });
 /*
-piltastkontroller
+piltastkontroller spiller 1
 */
 window.addEventListener('keydown', function(pressed) {
-  if(pressed.keyCode == 38){
+  if(pressed.key == 'w'){
     pressed.preventDefault();
     p1.vel.y = -550;
-  } else if (pressed.keyCode == 40) {
+  } else if (pressed.key == 'd') {
     pressed.preventDefault();
     p1.vel.y = 550;
   }
 });
 window.addEventListener('keyup', function(pressed) {
-  if(pressed.keyCode == 38 || pressed.keyCode == 40){
+  if(pressed.key == 'w' || pressed.key == 'd'){
     p1.vel.y = 0;
   }
 });
-// window.addEventListener('keydown', function(pressed) {
-//   if(pressed.keyCode == 40){
-//     console.log(pressed);
-//     p1.vel.y = 350;
-//   }
-// });
+
+// piltastkontroller spiller 2
+  window.addEventListener('keydown', function(pressed) {
+    if (pressed.keyCode == 38) {
+      pressed.preventDefault();
+      p2.vel.y = -550;
+    } else if (pressed.keyCode == 40) {
+      pressed.preventDefault();
+      p2.vel.y = 550;
+    }
+  });
+  window.addEventListener('keyup', function(pressed) {
+    if (pressed.keyCode == 38 || pressed.keyCode == 40) {
+      p2.vel.y = 0;
+    }
+  });
+
 
 
 // reset game & score
